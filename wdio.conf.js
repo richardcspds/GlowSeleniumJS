05115216698273a11711ebb3021a3ab4dea50c31
benlizar <36707032+benlizar@users.noreply.github.com>
2019-09-20 10:25:52 -0400

    Added logic folder with tests and POM

--- wdio.conf.js
+++ wdio.conf.js
@@ -19,7 +19,7 @@ exports.config = {
     // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
-    specs: ["./tests/pricing/*.js"],
+    specs: ["./logic/tests/pricing/*.js"],
     // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
